5dc5289143a36b574af32aeeef6988fa1c2ed708
Galinsxi <99427925+Galinsxi@users.noreply.github.com>
2023-04-15 00:50:06 +0300

    ideas.php
    This code is similar to the original code, but with some improvements:
    
    1. Added comments to explain what each section of the code does
    2. Refactored the code to use functional programming constructs such as array_map and anonymous functions

---
+++ ideas.php
@@ -0,0 +1,75 @@
+<?php
+// Set the response content type to JSON
+header('Content-Type:application/json');
+
+// Set your OpenAI API key
+$OPENAI_API_KEY = 'my API key goes here';
+
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  
+  // Get the topic from the POST parameters
+  $topic = isset($_POST['i']) ? htmlspecialchars($_POST['i']) : '';
+  
+  // Check if the topic is not empty
+  if ($topic != '') {
+    
+    // Initialize a cURL session to the OpenAI API
+    $ch = curl_init();
+    
+    // Set the cURL options for the API request
+    curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/completions');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      'Content-Type: application/json',
+      'Authorization: Bearer ' . $OPENAI_API_KEY ?? '',
+    ]);
+    $data = array(
+      'model' => 'text-davinci-003',
+      'prompt' => 'list 10 content ideas related to "'.$topic.'"',
+      'temperature' => 0.7,
+      'max_tokens' => 256,
+      'top_p' => 1,
+      'frequency_penalty' => 0,
+      'presence_penalty' => 0
+    );
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    
+    // Execute the API request and get the response
+    $response = curl_exec($ch);
+    
+    // Close the cURL session
+    curl_close($ch);
+    
+    // Parse the JSON response from the OpenAI API
+    $res = json_decode($response);
+    
+    // Get the ideas from the API response
+    $ideas = [];
+    if (isset($res->choices) && is_array($res->choices) && count($res->choices) > 0) {
+      $ideas_text = str_replace("\n", "<!--DIVIDER-->", $res->choices[0]->text);
+      $ideas = array_map(function($idea) {
+        return preg_replace('/^\d+\.\s+/', '', $idea);
+      }, explode("<!--DIVIDER-->", $ideas_text));
+    }
+    
+    // Create the JSON response for the client
+    $json_arr = [
+      'status' => empty($ideas) ? 'error' : 'success',
+      'ideas' => $ideas,
+      'message' => empty($ideas) ? 'No ideas found' : 'Fetch success'
+    ];
+    
+  } else {
+    // Create an error JSON response if the topic is empty
+    $json_arr = [
+      'status' => 'error_params',
+      'message' => 'Invalid params'
+    ];
+  }
+  
+  // Return the JSON response to the client
+  echo json_encode($json_arr, JSON_FORCE_OBJECT);
+}
+?>
